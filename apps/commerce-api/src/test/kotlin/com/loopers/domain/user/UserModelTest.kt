f1edc9568ade53def0f8cb26a5edcd569e05171b
mansik <90mansik@gmail.com>
2026-05-11 02:09:08 +0900

    refactor: UserModelTest 클래스명 파일명과 일치하도록 수정

--- apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserModelTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/UserModelTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class UserTest {
+class UserModelTest {
     companion object {
         private const val LOGIN_ID = "user01"
         private const val PASSWORD = "Password1!"

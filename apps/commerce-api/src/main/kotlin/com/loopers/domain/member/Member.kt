bc2f0f64b952514c47ec4a3edeab8342df3bcd85
0AndWild <parkky3563@gmail.com>
2026-05-12 22:48:24 +0900

    feat: (green) Member 기본 생성자 이름 유효성 검증 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/member/Member.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/member/Member.kt
@@ -1,6 +1,8 @@
 package com.loopers.domain.member
 
 import com.loopers.domain.BaseEntity
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
@@ -34,4 +36,14 @@ class Member(
     @Column(nullable = false)
     var email: String = email
         protected set
+
+    init {
+        if (!name.matches(NAME_REGEX)) {
+            throw CoreException(ErrorType.BAD_REQUEST, "Name cannot contain special characters or numbers.")
+        }
+    }
+
+    companion object {
+        private val NAME_REGEX = Regex("^[A-Za-z가-힣]+$")
+    }
 }

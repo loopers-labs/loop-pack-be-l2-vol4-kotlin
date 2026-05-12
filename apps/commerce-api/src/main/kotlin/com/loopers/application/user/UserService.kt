696a6df1516adc1b108bfbf6f86f7e5aac6f6675
yuntasha <jaljayo85@gmail.com>
2026-05-13 00:51:59 +0900

    :sparkles: 회원가입 API 완료

--- apps/commerce-api/src/main/kotlin/com/loopers/application/user/UserService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/application/user/UserService.kt
@@ -1,10 +1,26 @@
 package com.loopers.application.user
 
 import com.loopers.domain.user.User
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
 import org.springframework.stereotype.Component
 
 @Component
-class UserService {
+class UserService(
+    private val userRepository: UserRepository,
+) {
     fun signup(user: User) {
+        if (existsByLoginId(user.loginId)) {
+            throw CoreException(ErrorType.BAD_REQUEST, "이미 존재하는 아이디입니다.")
+        }
+        userRepository.save(user)
     }
+
+    private fun existsByLoginId(loginId: String): Boolean =
+        try {
+            userRepository.findByLoginId(loginId)
+            true
+        } catch (e: CoreException) {
+            if (e.errorType == ErrorType.NOT_FOUND) false else throw e
+        }
 }

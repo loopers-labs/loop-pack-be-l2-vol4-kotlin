c0a10220ebc31273c0c2406cd9a485c0b7c03537
0AndWild <parkky3563@gmail.com>
2026-05-13 21:52:14 +0900

    test: (red) 패스워드에 생년월일 포함시 실패 케이스 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/member/PasswordPolicy.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/member/PasswordPolicy.kt
@@ -2,9 +2,10 @@ package com.loopers.domain.member
 
 import com.loopers.support.error.CoreException
 import com.loopers.support.error.ErrorType
+import java.time.LocalDate
 
 object PasswordPolicy {
-    fun validate(rawPassword: String) {
+    fun validate(rawPassword: String, birthDate: LocalDate) {
         if (rawPassword.length !in 8..16) {
             throw CoreException(ErrorType.BAD_REQUEST, "Password length must be between 8 and 16")
         }
--- apps/commerce-api/src/test/kotlin/com/loopers/domain/member/PasswordPolicyTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/member/PasswordPolicyTest.kt
@@ -2,20 +2,27 @@ package com.loopers.domain.member
 
 import com.loopers.support.error.CoreException
 import com.loopers.support.error.ErrorType
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import kotlin.test.assertEquals
+import java.time.LocalDate
 
 class PasswordPolicyTest {
+
+    private val defaultBirthDate: LocalDate = LocalDate.of(1988, 2, 1)
+
     @DisplayName("패스워드가 8자리 미만 16자리 초과면 실패")
     @ParameterizedTest
     @ValueSource(strings = ["pass", "12345678,12345678"])
     fun throwsBadRequest_whenPasswordLengthIsInvalid(rawPassword: String) {
         val result = assertThrows<CoreException> {
-            PasswordPolicy.validate(rawPassword = rawPassword)
+            PasswordPolicy.validate(
+                rawPassword = rawPassword,
+                birthDate = defaultBirthDate,
+            )
         }
 
         assertEquals(ErrorType.BAD_REQUEST, result.errorType)
@@ -26,7 +33,25 @@ class PasswordPolicyTest {
     fun throwsBadRequest_whenPasswordContainsUnSupportedChars() {
         val rawPassword = "패스워드1234"
         val result = assertThrows<CoreException> {
-            PasswordPolicy.validate(rawPassword = rawPassword)
+            PasswordPolicy.validate(
+                rawPassword = rawPassword,
+                birthDate = defaultBirthDate,
+            )
+        }
+
+        assertEquals(ErrorType.BAD_REQUEST, result.errorType)
+    }
+
+    @DisplayName("패스워드에 생년월일이 포함되면 실패")
+    @Test
+    fun throwsBadRequest_whenPasswordContainsBirthDate() {
+        val rawPassword = "pp880201!"
+
+        val result = assertThrows<CoreException> {
+            PasswordPolicy.validate(
+                rawPassword = rawPassword,
+                birthDate = defaultBirthDate,
+            )
         }
 
         assertEquals(ErrorType.BAD_REQUEST, result.errorType)

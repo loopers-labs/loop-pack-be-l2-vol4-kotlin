d336e1b66b32ef1cc9438c94c94f27d2d069525c
seondays <agseon9@gmail.com>
2026-06-09 18:08:34 +0900

    feat : 쿠폰 정책을 의미하는 Coupon 도메인 레이어 구현

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/coupon/Coupon.kt
@@ -0,0 +1,25 @@
+package com.loopers.domain.coupon
+
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
+
+class Coupon(
+    val id: Long? = null,
+    name: String,
+    val policy: DiscountPolicy,
+) {
+    var name: String = name
+        private set
+
+    init {
+        validateName(name)
+    }
+
+    fun discountOf(targetAmount: DiscountAmount): DiscountAmount = policy.discountOf(targetAmount)
+
+    companion object {
+        private fun validateName(name: String) {
+            if (name.isBlank()) throw CoreException(ErrorType.BAD_REQUEST, "쿠폰명은 비어있을 수 없습니다.")
+        }
+    }
+}

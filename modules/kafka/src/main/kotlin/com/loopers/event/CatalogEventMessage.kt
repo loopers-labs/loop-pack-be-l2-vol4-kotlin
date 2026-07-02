e7b9a5e5f152548b4e4ab54ded17d7f11846e1c2
0AndWild <parkky3563@gmail.com>
2026-07-02 18:21:23 +0900

    feat: 카탈로그 Kafka 이벤트 계약 추가

---
+++ modules/kafka/src/main/kotlin/com/loopers/event/CatalogEventMessage.kt
@@ -0,0 +1,19 @@
+package com.loopers.event
+
+import java.time.ZonedDateTime
+
+data class CatalogEventMessage(
+    val eventId: String,
+    val eventType: CatalogEventType,
+    val aggregateId: Long,
+    val productId: Long,
+    val brandId: Long,
+    val memberId: Long?,
+    val version: Long,
+    val occurredAt: ZonedDateTime,
+)
+
+enum class CatalogEventType {
+    PRODUCT_LIKED,
+    PRODUCT_UNLIKED,
+}
